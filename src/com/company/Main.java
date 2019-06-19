5bc9eecc2d64470ca1d85806bf221817d3ebfdcb
arahman1976 <ar.nj.usa@gmail.com>
2019-06-19 17:00:11 -0400

    in process

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collection;
+
+        public class Main {
+
+            public static void main(String[] args) {
+                // write your code here
+
+                //   Write a Java program to create a new array list to hold colors.
+
+                ArrayList<String> colors = new ArrayList<>();
+                colors.add("blue");
+                colors.add("green");
+                colors.add("orange");
+
+                System.out.println("Ex#1&2" + "\n");
+                for (String items : colors)
+                    System.out.println(items);
+
+                // declaring an array list
+
+                // print value at index 1
+                System.out.println("\n" + "**Ex#3 Print value at index1** ");
+                System.out.println(colors.get(1) + "\n");
+
+                //Using the array list above, prompt the user for one new color, and add that color to the array list.
+                System.out.println("\n" + "**Ex#4 new color** ");
+                System.out.println(" Enter a new color");
+                Scanner input = new Scanner(System.in);
+                colors.add();
+
+                while (true) {
+                    input.add(in.next
+                            System.out.println(input.toString());
+
+
+                }
+            }
+        }
